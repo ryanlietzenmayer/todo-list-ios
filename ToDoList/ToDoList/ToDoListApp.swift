0ed926747f7885519234e1363e6536fb1bb3f5f1
Ryan Lietzenmayer <ryan.lietz@gmail.com>
2026-06-04 15:08:05 +0800

    xcode project setup

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Meng Wang on 6/4/26.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoList/ToDoListTests/ToDoListTests.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoListTests.swift
+//  ToDoListTests
+//
+//  Created by Meng Wang on 6/4/26.
+//
+
+import Testing
+
+struct ToDoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
