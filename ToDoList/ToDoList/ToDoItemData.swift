4228c099ca9e90ab45eb663b3faeddad1d566b17
Ryan Lietzenmayer <ryan.lietz@gmail.com>
2026-06-05 11:19:15 +0800

    models

---
+++ ToDoList/ToDoList/ToDoItemData.swift
@@ -0,0 +1,47 @@
+//
+//  ToDoItemData.swift
+//  ToDoList
+//
+//  Created by Meng Wang on 6/5/26.
+//
+
+import Foundation
+
+public struct ToDoItemsData: Decodable {
+    let items: [ToDoItemData]?
+}
+
+public struct ToDoItemData: Decodable {
+    let id: Int?
+    let taskDescription: String?
+    let createdDate: Date?
+    let dueDate: Date?
+    let completed: Bool?
+}
+
+public struct ToDoItem {
+    let id: Int
+    let taskDescription: String
+    let createdDate: Date
+    let dueDate: Date?
+    let completed: Bool
+
+    init(id: Int, taskDescription: String, createdDate: Date, dueDate: Date?, completed: Bool) {
+        self.id = id
+        self.taskDescription = taskDescription
+        self.createdDate = createdDate
+        self.dueDate = dueDate
+        self.completed = completed
+    }
+}
+
+extension ToDoItem {
+    init?(from todoItemData: ToDoItemData) {
+        guard let id = todoItemData.id, let taskDescription = todoItemData.taskDescription else { return nil }
+        self.init(id: id,
+                  taskDescription: taskDescription,
+                  createdDate: todoItemData.createdDate ?? Date.now,
+                  dueDate: todoItemData.dueDate,
+                  completed: todoItemData.completed ?? false)
+    }
+}
